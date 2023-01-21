11eb0f401127875fbf796cffb53cc873bc5f4283
Gavin-1001 <gavinshelley10@gmail.com>
2023-01-21 13:33:59 +0000

    EventEmitter Section 2 video 10

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Course} from "../model/course";
 
 @Component({
@@ -14,5 +14,12 @@ export class CourseCardComponent {
     @Input()
     course:Course; //inputs the whole courseModel to access properties instead of just a single property like title
 
+    @Output()
+    courseSelected = new EventEmitter<Course>();
 
+    onCourseViewed() {
+        console.log("Working");
+        this.courseSelected.emit(this.course); //this emits a custom event when the button on each card is selected
+        //prints out the course[0] payload from the custom event
+    }
 }
